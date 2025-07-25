27f903949440733f197049387dba8c1d84ec6ac6
PGWan <PGWan@gmail.com>
2025-07-25 19:59:14 +0800

    更新preview环境

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 // make-cv/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'config/*', dest: 'config' },
+      ],
+    }),
+  ],
   
   server: {
     port: 3018,
@@ -20,8 +28,9 @@ export default defineConfig({
       console.log(`服务运行成功：http://${domain}:${port}`);
     },
   },
-  // preview: {
-  //   port: 3018,
-  //   host: '0.0.0.0',
-  // },
+  preview: {
+    port: 3018,
+    host: '0.0.0.0',
+    // 更多功能需要的话自己扩展
+  },
 })
